328aca3d65d3714e4c2382af72324b800b0d020e
Jo <j.lou123@hotmail.com>
2019-11-16 01:33:40 -0500

    Splash screen, icon, bottom nav bar mostly set up, and main colors in stored in a constant

--- app/App.js
+++ app/App.js
@@ -1,16 +1,52 @@
+//Library Imports
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
-
+import Icon from 'react-native-vector-icons/FontAwesome5';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { createAppContainer } from 'react-navigation';
+
+//Component Imports
 import Overview from './Overview/Overview';
 import Account from './Account/Account';
+import PlantSettings from './PlantSettings/PlantSettings';
+import Metrics from './Metrics/Metrics';
 
-const TabNavigator = createBottomTabNavigator({
-  Overview: Overview,
-  Metrics: Metrics, 
-  PlantSettings: PlantSettings, 
-  Account: Account
-});
+//Style Imports
+import COLORS from './styles';
+
+const TabNavigator = createBottomTabNavigator(
+  {
+    Overview: Overview,
+    Metrics: Metrics, 
+    PlantSettings: PlantSettings, 
+    Account: Account
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, horizontal, tintColor }) => {
+        const { routeName } = navigation.state;
+        let iconName;
+        if (routeName === 'Overview') {
+          iconName = 'home';
+        } else if (routeName === 'PlantSettings') {
+          iconName = 'seedling';
+        } else if (routeName === 'Metrics') {
+          iconName = 'chart-line';
+        } else if (routeName === 'Account') {
+          iconName = 'wrench';
+        }
+
+        return <Icon name={iconName} size={25} color={tintColor} />;
+      },
+    }),
+    tabBarOptions: {
+      activeTintColor: COLORS.white,
+      inactiveTintColor: COLORS.green3,
+      style: {
+        backgroundColor: COLORS.green5,
+      }
+    },
+  }
+);
 
 export default createAppContainer(TabNavigator);
---
+++ app/styles.js
@@ -0,0 +1,19 @@
+//GLOBAL STYLESHEET
+
+import { StyleSheet } from 'react-native';
+
+const COLORS = {
+    green3: '#7eea6c',
+    green5: '#00dd00', //Main Theme colour
+    green7: '#00b700',
+    grey1: '#f8f9fa', //Background behind cards
+    grey3: '#d5dde5', 
+    grey5: '#aebecd',
+    grey7: '6e7a8a', //Subheadings
+    grey9: '#202833', //Important headings
+    white: '#ffffff', //Cards 
+    yellow: '#ffc927', //Warning
+    red: '#B71C1C' //Errors
+}
+
+export default COLORS;
