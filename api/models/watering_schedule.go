83eb81c73c9182458eeb8098859c102cf157aeb4
2J <jj@jj.ai>
2020-01-27 20:43:52 -0500

    API endpoints for watering schedules

--- api/models/watering_schedule.go
+++ api/models/watering_schedule.go
@@ -16,27 +16,36 @@ type WateringSchedule struct {
 	Active   bool     `json:"active" validate:"required"`
 }
 
-/*
-
-func (wateringSchedule *WateringSchedule) Validate() error {
-	err := nil
-	// TODO
+func (wateringSchedule *WateringSchedule) Validate() (err error) {
 	return err
 }
 
-func (wateringSchedule *WateringSchedule) Create() error {
-	// TODO
-	return nil
+func (wateringSchedule *WateringSchedule) Create() (int, error) {
+	db := database.Open()
+	defer database.Close(db)
+
+	insForm, err := db.Prepare("INSERT INTO wateringSchedules (`plantId`, `time`, `amount`, `repeatDays`, `repeatEndDate`, `active`) VALUES (?,?,?,?,?,?)")
+	if err != nil {
+		return 0, err
+	}
+
+	res, err := insForm.Exec(
+		wateringSchedule.PlantID,
+		wateringSchedule.Schedule.Time,
+		wateringSchedule.Amount,
+		wateringSchedule.Schedule.RepeatDays,
+		wateringSchedule.Schedule.RepeatEndDate,
+		true,
+	)
+
+	if err != nil {
+		return 0, err
+	}
+
+	id, err := res.LastInsertId()
+
+	return int(id), err
 }
-
-
-func (wateringSchedule *WateringSchedule) Get(filter string) error {
-
-	query += " " + filter
-	rows, err := db.Query(queryd)
-	return nil
-}
-*/
 
 func (wateringSchedule *WateringSchedule) getRow(rows *sql.Rows) error {
 	err := rows.Scan(&wateringSchedule.ID,
@@ -91,4 +100,20 @@ func GetWateringSchedulesByPlantId(plantId int) ([]WateringSchedule, error) {
 	}
 
 	return res, nil
+}
+
+func DeleteWateringSchedule(scheduleId int) error {
+	db := database.Open()
+	defer database.Close(db)
+
+	delForm, err := db.Prepare("DELETE FROM wateringSchedules WHERE id = ?")
+	if err != nil {
+		return err
+	}
+
+	_, err = delForm.Exec(
+		scheduleId,
+	)
+
+	return err
 }
